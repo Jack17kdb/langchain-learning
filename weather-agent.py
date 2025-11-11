11bcf9e8b761f8f60bbf891a0cbdeaed17368e48
Jack17kdb <jack@kali.Johnson.com>
2025-11-11 19:43:16 +0300

    weather agent

---
+++ weather-agent.py
@@ -0,0 +1,28 @@
+import requests
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain.tools import tool
+
+load_dotenv()
+
+@tool("get_weather", description='Return weather information for a given city', return_direct=False)
+def get_weather(city: str):
+	response = requests.get(f"https://wttr.in/{city}?format=j1")
+	return response.json()
+
+agent = create_agent(
+	model = 'gpt-4.1-mini',
+	tools = [get_weather],
+	system_prompt = "You are a helpful weather assistant"
+)
+
+response = agent.invoke({
+	"messages": [
+		{
+			"role": "user", "content": "What is the weather in Kenya?"
+		}
+	]
+})
+
+print(response, "\n")
+print(response['messages'][-1].content)
